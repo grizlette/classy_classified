7c49d266d55dedb78bbed9e999a0b8d5c93d51c2
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-06-24 11:44:42 -0600

    created controller for Item and views associated with it

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,55 @@
+class ItemsController < ApplicationController
+
+  before_action :find_item, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    if @item.save(item_params)
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to items_path
+    else
+      flash[:error] = "Item wasn't destroyed, see error logs and try again!"
+      redirect_to items_path
+    end
+  end
+
+
+
+  private
+    def find_item
+      @item = item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:item_name, :item_price, :item_description, :seller_name, :seller_phone_number, :seller_email, :seller_location, :category_id)
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150624172406) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "category"
+    t.string   "sub_category"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "item_name"
@@ -30,15 +34,6 @@ ActiveRecord::Schema.define(version: 20150624172406) do
     t.integer  "category_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-end
-  create_table "categories", force: :cascade do |t|
-    t.string   "category"
-    t.string   "sub_category"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-
   end
 
 end
-
-
