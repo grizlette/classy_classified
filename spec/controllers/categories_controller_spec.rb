b3b13e07e85feeb2088c601dc2902504fc7c7d27
Megan Grisley <megangrisley@gmail.com>
2015-06-25 08:42:40 -0600

    added navbar

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
+  let(:category){Category.create!(category_name: 'New Category', sub_category: 'New sub_category')}
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -10,11 +12,16 @@ RSpec.describe CategoriesController, type: :controller do
   end
 
   describe "GET #edit" do
-    it "returns http success" do
-      get :edit
+    it "does edit" do
+      get :edit, id: category.id
       expect(response).to have_http_status(:success)
     end
+
+    it "doesn't edit" do
+      get :edit, id: nil
+      expect(flash[:error]).to be_present
   end
+end
 
   describe "GET #new" do
     it "returns http success" do
@@ -24,9 +31,39 @@ RSpec.describe CategoriesController, type: :controller do
   end
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
+    it "does show" do
+      get :show, id: category.id
       expect(response).to have_http_status(:success)
+    end
+
+    it "doesn't show" do
+      get :show, id: 5000
+      expect(flash[:error]).to be_present
+    end
+  end
+
+  describe "POST #create" do
+    it "does create" do
+      post :create, category: {category_name: 'Create Category', sub_category: 'Create Subcategory'}
+      expect(response).to have_http_status(:redirect)
+      expect(Item.all.count).to eq(1)
+    end
+
+    it "doesn't create" do
+      post :create, category: {category_name: nil, sub_category: 'Create Subcategory'}
+      expect(Category.all.count).to eq(0)
+    end
+  end
+
+  describe "PUT update" do
+    it "does update" do
+      put :update, id: category.id, category: {category_name: 'New Category Name', sub_category: 'New Subcategory Name'}
+      expect(Category.all.reload.first.category_name).to eq('New Category Name')
+    end
+
+    it "doesn't update" do
+      put :update, id: category.id, category: {category_name: nil}
+      expect(flash[:error]).to be_present
     end
   end
 
